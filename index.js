9ce8f5d8ca843b6fecab9b0c3573d8aba0cae9b7
ale-strada <79310707+ale-strada@users.noreply.github.com>
2021-05-24 19:32:37 -0300

    primer commit

--- index.js
+++ index.js
@@ -25,6 +25,21 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  let ulEl = document.querySelector(".lista");
+  while (ulEl.firstChild) {
+    ulEl.removeChild(ulEl.firstChild);
+  }
 
+  cosasQueAprendimos.forEach((element) => {
+    const newLiEl = document.createElement("li");
+    var newContent = document.createTextNode(element.tema);
+    newLiEl.className = element.class;
+    newLiEl.appendChild(newContent);
+    ulEl.appendChild(newLiEl);
+    console.log(newLiEl);
+  });
+
+  console.log(ulEl);
+}
 main();
